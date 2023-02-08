dc6535b998b89b59d846b81d782c3b2f337600cb
LizaPoperechna <poperechnayaliza@gmail.com>
2023-02-08 14:47:30 -0500

    добавила хук useLocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,25 @@
-import { useState, useEffect} from "react";
+import { useState} from "react";
 import { nanoid } from 'nanoid';
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactList } from "./ContactList/ContactList";
 import { Filter } from "./Filter/Filter";
 import { H1, H2, P } from "./App.styled";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 
-
+const initialContacts = [
+  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+];
 
 export const App = () => {
 
-  const [contacts, setContacts] = useState([]);
+  const [contacts, setContacts] = useLocalStorage('contacts', initialContacts);
   const [filter, setFilter] = useState('');
 
-  useEffect(() => {
-      const initialContacts = [
-        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-      ];
-          
-      const savedContacts = localStorage.getItem('contacts');
-      
-      if (savedContacts !== null) {
-        setContacts(prev => [...prev, ...JSON.parse(savedContacts)])
-      } else setContacts(prev => [...prev, ...initialContacts])
-      }, []);
-
-
-  useEffect(
-    prevState => {localStorage.setItem('contacts', JSON.stringify(contacts))}, [contacts]);
-
-
-
+  
   const addContact = (name, number) => {
     if (contacts.find(option => option.name.toLowerCase() === name.toLowerCase()))
           { alert(`${name} is already in contacts list`); 
